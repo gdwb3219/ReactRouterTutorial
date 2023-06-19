2629c1aded838b3f9795a500e04f050c309db24e
gdwb3219 <gdwb3219@gmail.com>
2023-06-20 00:35:31 +0900

    UnderStanding Out React Router Outlet Component

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Root from './routes/root';
+import ErrorPage from './error-page';
+import Contact from './routes/contact';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Hello world!</div>,
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: 'contacts/:contactId',
+        element: <Contact />,
+      },
+    ],
   },
 ]);
 
