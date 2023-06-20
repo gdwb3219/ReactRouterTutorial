1f051b4d8b8de6e1075418874c4e63e6bd2c0f29
gdwb3219 <gdwb3219@gmail.com>
2023-06-21 00:54:25 +0900

    data writes, HTML forms

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Root from './routes/root';
-import ErrorPage from './error-page';
-import Contact from './routes/contact';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Root, { loader as rootLoader } from "./routes/root";
+import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
     children: [
       {
-        path: 'contacts/:contactId',
+        path: "contacts/:contactId",
         element: <Contact />,
       },
     ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
