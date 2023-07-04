ea1eddf3ddc27a865009d93615458f761d8cee68
gdwb3219 <gdwb3219@gmail.com>
2023-07-04 23:46:17 +0900

    Difficult at Action/Mutation Discusstion

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,8 @@ import Root, {
   action as rootAction,
 } from './routes/root';
 import ErrorPage from './error-page';
-import Contact from './routes/contact';
+import Contact, { loader as contactLoader } from './routes/contact';
+import EditContact, { action as editAction } from './routes/edit';
 
 const router = createBrowserRouter([
   {
@@ -22,6 +23,13 @@ const router = createBrowserRouter([
         // : has special meaning
         path: 'contacts/:contactId',
         element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: 'contacts/:contactId/edit',
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
       },
     ],
   },
