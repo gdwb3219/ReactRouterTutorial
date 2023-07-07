5934c053e17c134017a6bbc5fd498bbf89e219ed
gdwb3219 <gdwb3219@gmail.com>
2023-07-08 01:43:59 +0900

    Learning until Destroy

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,9 @@ import Root, {
   action as rootAction,
 } from './routes/root';
 import ErrorPage from './error-page';
-import Contact from './routes/contact';
+import Contact, { loader as contactLoader } from './routes/contact';
+import EditContact, { action as editAction } from './routes/edit';
+import { action as destroyAction } from './routes/destroy';
 
 const router = createBrowserRouter([
   {
@@ -22,6 +24,18 @@ const router = createBrowserRouter([
         // : has special meaning
         path: 'contacts/:contactId',
         element: <Contact />,
+        loader: contactLoader,
+      },
+      {
+        path: 'contacts/:contactId/edit',
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+      {
+        path: 'contacts/:contactId/destroy',
+        action: destroyAction,
+        errorElement: <div>Oops! There was an error.</div>,
       },
     ],
   },
